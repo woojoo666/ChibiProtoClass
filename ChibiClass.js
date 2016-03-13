fdd292e0a52cbc6bc54eb95354f79e2474cbeeee
Max Woo <maxithewoowoo@gmail.com>
2016-03-12 22:39:26 -0800

    ChibiClass, more traditional classing method
    ChibiClass itself is not the prototype for construct()
    ChibiClass.prototype stores the prototype used for construct()
    created instances do not have the construct() and extend() methods
    ChibiClass.extend creates a new ChibiClass with a different prototype
    shorthand: myChibiClass(arg) invokes myChibiClass.construct(arg)
    test.html added tests for ChibiClass

---
+++ ChibiClass.js
@@ -0,0 +1,21 @@
+var ChibiClass = (function createChibi (proto) {
+	function chibi () {
+		return chibi.construct.apply(chibi, arguments);
+	}
+	chibi.proto = proto;
+	chibi.construct = function () {
+		var instance = Object.create(this.proto);
+		instance.constructor.apply(instance, arguments);
+		return instance;
+	};
+	chibi.extend = function (props) {
+		var newproto = Object.create(this.proto);
+		for (var prop in props) {
+			if(props.hasOwnProperty(prop)) {
+				newproto[prop] = props[prop];
+			}
+		}
+		return createChibi(newproto);
+	};
+	return chibi;
+})({});
